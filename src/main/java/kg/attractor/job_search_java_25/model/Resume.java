b916b73fc090a606f2e7acf8667d4d54703b06f0
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-07-09 00:51:44 +0600

    models

---
+++ src/main/java/kg/attractor/job_search_java_25/model/Resume.java
@@ -0,0 +1,19 @@
+package kg.attractor.job_search_java_25.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class Resume {
+    private Long id;
+    private Long applicantId;
+    private String name;
+    private Long categoryId;
+    private Double salary;
+    private Boolean isActive;
+    private Timestamp createdDate;
+    private Timestamp updatedDate;
+}
