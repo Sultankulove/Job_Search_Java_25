491d7d1e1084748e97d7111bc62dc54d01a1c163
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-07-15 16:13:48 +0600

    create avatar dto

---
+++ src/main/java/kg/attractor/job_search_java_25/dto/AvatarDto.java
@@ -0,0 +1,12 @@
+package kg.attractor.job_search_java_25.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+public class AvatarDto {
+    private MultipartFile file;
+    private long avatarId;
+}
