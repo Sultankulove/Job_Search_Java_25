3fce2e6dada2063e8770286a8558d9d06277d426
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-07-18 14:49:24 +0600

    create dao

---
+++ src/main/java/kg/attractor/job_search_java_25/dao/UserDao.java
@@ -0,0 +1,68 @@
+package kg.attractor.job_search_java_25.dao;
+
+import kg.attractor.job_search_java_25.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class UserDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public List<User> findAll() {
+        String sql = "SELECT * FROM users";
+        return namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+    }
+
+
+    public User findByName(String name) {
+        String sql = "select * from users where name = :name";
+        return namedParameterJdbcTemplate.queryForObject(sql,
+                Map.of("name", name), new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public User findByPhoneNumber(String phoneNumber) {
+        String sql = "select * from users where phone_number = :phoneNumber";
+        return namedParameterJdbcTemplate.queryForObject(sql,
+                Map.of("phoneNumber", phoneNumber), new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public User findByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = :email";
+        return namedParameterJdbcTemplate.queryForObject(sql, Map.of("email", email), new BeanPropertyRowMapper<>(User.class));
+    }
+
+
+    public boolean existsByEmail(String email) {
+        String sql = "SELECT COUNT(*) FROM users WHERE email = :email";
+        Integer count = namedParameterJdbcTemplate.queryForObject(sql, Map.of("email", email), Integer.class);
+        return count != null && count > 0;
+    }
+
+    public void save(User user) {
+        String sql = "INSERT INTO users (name, surname, age, email, password, phone_number, avatar, account_type) VALUES (:name, :surname, :age, :email, :password, :phone, :avatar, :type)";
+        Map<String, Object> params = Map.of(
+                "name", user.getName(),
+                "surname", user.getSurname(),
+                "age", user.getAge(),
+                "email", user.getEmail(),
+                "password", user.getPassword(),
+                "phone", user.getPhoneNumber(),
+                "avatar", user.getAvatar(),
+                "type", user.getAccountType()
+        );
+        namedParameterJdbcTemplate.update(sql, params);
+    }
+
+    public void deleteById(int id) {
+        String sql = "DELETE FROM users WHERE id = :id";
+        namedParameterJdbcTemplate.update(sql, Map.of("id", id));
+    }
+}
