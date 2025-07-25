7c4b2c6b07c6aec67e842f55e1909d408aad8eaf
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-07-25 08:55:10 +0600

    added for dto validation

--- src/main/java/kg/attractor/job_search_java_25/dto/UserDto.java
+++ src/main/java/kg/attractor/job_search_java_25/dto/UserDto.java
@@ -1,5 +1,6 @@
 package kg.attractor.job_search_java_25.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +12,23 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserDto {
     private Long id;
+
+    @NotBlank(message = "Имя не должно быть пустым")
     private String name;
     private String surname;
+
+    @NotNull(message = "Возраст обязателен")
+    @Min(value = 16, message = "Возраст должен быть не менее 16 лет")
+    @Max(value = 128, message = "Возраст не должен превышать 128 лет")
     private Short age;
+
+    @NotBlank(message = "Email обязателен")
+    @Email(message = "Неверный формат email")
     private String email;
+
+    @NotBlank(message = "Номер телефона обязателен")
     private String phoneNumber;
+
     private String avatar;
-    private String account_type;
+    private String accountType;
 }
