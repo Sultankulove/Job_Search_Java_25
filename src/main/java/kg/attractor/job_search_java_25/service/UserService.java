33a6bea692895c5d0a7b70c97b9b279148702061
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-07-25 15:54:57 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/service/UserService.java
+++ src/main/java/kg/attractor/job_search_java_25/service/UserService.java
@@ -1,25 +1,14 @@
 package kg.attractor.job_search_java_25.service;
 
 
-import kg.attractor.job_search_java_25.dto.AvatarDto;
 import kg.attractor.job_search_java_25.dto.UserDto;
-import lombok.SneakyThrows;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
+import kg.attractor.job_search_java_25.model.User;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public interface UserService {
 
 
-    void createUser();
+    User createUser(UserDto userDto);
 
     void editUser(Long id, UserDto userDto);
 
