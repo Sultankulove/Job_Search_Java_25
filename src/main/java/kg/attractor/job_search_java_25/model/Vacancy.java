f245f089ba62def421953f6b0f1c31fbda95cbfa
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-07 10:51:33 +0600

    model

---
+++ src/main/java/kg/attractor/job_search_java_25/model/Vacancy.java
@@ -0,0 +1,22 @@
+package kg.attractor.job_search_java_25.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Vacancy {
+    private Long id;
+    private String name;
+    private String description;
+    private Long categoryId;
+    private Float salary;
+    private Integer expFrom;
+    private Integer expTo;
+    private Boolean isActive;
+    private Long authorId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
