7015bc3253a5e6d5ecd4b4f727d8256501333a79
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-07 10:47:02 +0600

    add /profile

--- src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
@@ -2,19 +2,18 @@ package kg.attractor.job_search_java_25.controller;
 
 import kg.attractor.job_search_java_25.dto.RegistrationRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api")
+@RequestMapping("api/profile")
 public class AuthController {
 
     @PostMapping("register")
-    public String register(@RequestBody RegistrationRequestDto RRD) {
+    public String register(@RequestBody RegistrationRequestDto rrd) {
         // Регистрация: Имя! Email! NumberPhone! Avatar(if not default)
         return "OK";
     }
+
+
 }
