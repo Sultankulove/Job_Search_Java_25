d69c8cf70153c642920df5ab9bb18ab57421d57c
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-07 10:48:12 +0600

    dto VacancyShortDto (name, updateTime)

---
+++ src/main/java/kg/attractor/job_search_java_25/dto/VacancyShortDto.java
@@ -0,0 +1,17 @@
+package kg.attractor.job_search_java_25.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VacancyShortDto {
+    private String name;
+    private LocalDateTime updateTime;
+}
