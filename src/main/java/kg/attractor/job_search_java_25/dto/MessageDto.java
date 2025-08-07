878a129052365f19993a72a5851d1a38ba789bae
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-07 10:52:44 +0600

    blank dto

---
+++ src/main/java/kg/attractor/job_search_java_25/dto/MessageDto.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search_java_25.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+//@AllArgsConstructor
+public class MessageDto {
+}
