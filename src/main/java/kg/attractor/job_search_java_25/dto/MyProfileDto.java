1a334f3bb8cd5debed4f18e06f7df6bb4f3f5dab
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-08 08:15:57 +0600

    profile

--- src/main/java/kg/attractor/job_search_java_25/dto/MyProfileDto.java
+++ src/main/java/kg/attractor/job_search_java_25/dto/MyProfileDto.java
@@ -3,17 +3,17 @@ package kg.attractor.job_search_java_25.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@RequestMapping
 @AllArgsConstructor
-public class EditProfileDto {
+@NoArgsConstructor
+public class MyProfileDto {
     private String name;
     private String surname;
     private Byte age;
     private String email;
-    private String password;
     private String phoneNumber;
+    private String avatar;
 }
