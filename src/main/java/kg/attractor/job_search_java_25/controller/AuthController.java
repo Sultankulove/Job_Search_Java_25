ae9cfc6b453fb988456f47e52b752087413dd780
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-08 22:25:41 +0600

    fix

--- src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
@@ -1,25 +1,25 @@
 package kg.attractor.job_search_java_25.controller;
 
-import kg.attractor.job_search_java_25.dao.UserDao;
+import jakarta.validation.Valid;
 import kg.attractor.job_search_java_25.dto.RegistrationRequestDto;
 import kg.attractor.job_search_java_25.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("api/profile")
 public class AuthController {
     private final UserService userService;
 
     @PostMapping("register")
-    public ResponseEntity<RegistrationRequestDto> register(@RequestBody RegistrationRequestDto rrd) {
-        // Регистрация: Имя! Email! NumberPhone! Avatar(if not default)
+    public ResponseEntity<RegistrationRequestDto> register(@RequestBody @Valid RegistrationRequestDto rrd) {
+        log.info("POST /api/profile/register - попытка регистрации email={}", rrd.getEmail());
         userService.registration(rrd);
-        return new ResponseEntity<>(rrd, HttpStatus.OK);
+        return ResponseEntity.ok(rrd);
     }
-
 
 }
