44a8e98313a6eae9180f0565f282a09a5a57f2d1
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-08 08:08:41 +0600

    registration

--- src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
+++ src/main/java/kg/attractor/job_search_java_25/controller/AuthController.java
@@ -1,18 +1,24 @@
 package kg.attractor.job_search_java_25.controller;
 
+import kg.attractor.job_search_java_25.dao.UserDao;
 import kg.attractor.job_search_java_25.dto.RegistrationRequestDto;
+import kg.attractor.job_search_java_25.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/profile")
 public class AuthController {
+    private final UserService userService;
 
     @PostMapping("register")
-    public String register(@RequestBody RegistrationRequestDto rrd) {
+    public ResponseEntity<RegistrationRequestDto> register(@RequestBody RegistrationRequestDto rrd) {
         // Регистрация: Имя! Email! NumberPhone! Avatar(if not default)
-        return "OK";
+        userService.registration(rrd);
+        return new ResponseEntity<>(rrd, HttpStatus.OK);
     }
 
 
