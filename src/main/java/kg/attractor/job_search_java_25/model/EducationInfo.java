071a79c939436b2ded314df4032ad62a08343abc
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-08 08:16:07 +0600

    education

---
+++ src/main/java/kg/attractor/job_search_java_25/model/EducationInfo.java
@@ -0,0 +1,18 @@
+package kg.attractor.job_search_java_25.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+public class EducationInfo {
+    private Long id;
+    private Long resumeId;
+    private String institution;
+    private String program;
+    private Date startDate;
+    private Date endDate;
+    private String degree;
+}
