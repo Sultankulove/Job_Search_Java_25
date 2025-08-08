f9c8d3257535178f6a636518480fe4138b7f637a
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-08 22:27:28 +0600

    delete

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
--- заполнение таблиц
-
-delete from users where email = 'qwe@qwe.qwe';
-
-insert into users(name, surname, age, email, password, phone_number, avatar, account_type, enabled) values
-                      ('Elturan', 'Sultankulov', 22, 'qwe@qwe.qwe', 'qwerty', '+996771653653', '', 'EMPLOYER', true);
