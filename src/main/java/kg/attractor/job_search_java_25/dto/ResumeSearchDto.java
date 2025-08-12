9b2d5be0ccc5e62fe964e734a25784a58a2155b5
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-12 16:59:14 +0600

    fix

---
+++ src/main/java/kg/attractor/job_search_java_25/dto/ResumeSearchDto.java
@@ -0,0 +1,14 @@
+package kg.attractor.job_search_java_25.dto;
+
+import lombok.Data;
+
+@Data
+public class ResumeSearchDto {
+    private Long categoryId;
+    private Float minSalary;
+    private Float maxSalary;
+    private String searchTerm;
+    private String sort;
+    private Integer page;
+    private Integer size;
+}
