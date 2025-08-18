2327dfff3012dfbe752672b2210f9adc54c65afb
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-08-18 15:34:46 +0600

    new

---
+++ src/main/java/kg/attractor/job_search_java_25/service/CategoryService.java
@@ -0,0 +1,9 @@
+package kg.attractor.job_search_java_25.service;
+
+import kg.attractor.job_search_java_25.dto.CategoryDto;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryDto> findAll();
+}
