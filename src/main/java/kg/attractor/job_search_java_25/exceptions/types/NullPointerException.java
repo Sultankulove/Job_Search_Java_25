7c9e6e49e18b95ce8c576ab60c643f24f74e1dcf
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-04 11:41:31 +0600

    exception NullPointerException.java

---
+++ src/main/java/kg/attractor/job_search_java_25/exceptions/types/NullPointerException.java
@@ -0,0 +1,7 @@
+package kg.attractor.job_search_java_25.exceptions.types;
+
+public class NullPointerException extends RuntimeException {
+    public NullPointerException(String message) {
+        super(message);
+    }
+}
