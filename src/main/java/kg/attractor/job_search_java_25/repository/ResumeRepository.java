2e7c73e099be5e70fb1bf2fb3fb721e51375520f
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-04 11:50:40 +0600

    edit repo

--- src/main/java/kg/attractor/job_search_java_25/repository/ResumeRepository.java
+++ src/main/java/kg/attractor/job_search_java_25/repository/ResumeRepository.java
@@ -1,11 +1,13 @@
 package kg.attractor.job_search_java_25.repository;
 
 
-import jakarta.validation.constraints.*;
 import kg.attractor.job_search_java_25.dto.ResumeDto;
 import kg.attractor.job_search_java_25.dto.ResumeListViewDto;
 import kg.attractor.job_search_java_25.dto.ResumeShortDto;
 import kg.attractor.job_search_java_25.model.Resume;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,11 +17,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
+    Page<Resume> findByCategory_Id(Long categoryId, Pageable pageable);
+
+    Page<Resume> findAll(Pageable pageable);
 
     List<Resume> findAllByApplicant_Id(Long applicantId);
 
