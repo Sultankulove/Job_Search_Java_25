8b3e5bf45f0ec01dc43f7924e0515c163bd0703a
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-06 12:36:07 +0600

    create ContactInfoDto

---
+++ src/main/java/kg/attractor/job_search_java_25/dto/ContactInfoDto.java
@@ -0,0 +1,22 @@
+package kg.attractor.job_search_java_25.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactInfoDto {
+
+    private Long id;
+
+    private Long type_id;
+
+    private Long resume_id;
+
+    private String contactValue;
+
+}
