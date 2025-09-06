7a7bdd97a4b372744ea0f41b80a7227ab4e8e201
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-06 12:32:07 +0600

    added     @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/kg/attractor/job_search_java_25/model/Category.java
+++ src/main/java/kg/attractor/job_search_java_25/model/Category.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", length = 128, unique = true)
