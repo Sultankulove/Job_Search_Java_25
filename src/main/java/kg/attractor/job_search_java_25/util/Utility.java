2f1338f2d7a1b280b782ac302725946226d5befd
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-06 17:10:42 +0600

    Utility.java

---
+++ src/main/java/kg/attractor/job_search_java_25/util/Utility.java
@@ -0,0 +1,12 @@
+package kg.attractor.job_search_java_25.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class Utility {
+    private Utility() {}
+
+    public static String makeSiteUrl(HttpServletRequest request) {
+        String url = request.getRequestURL().toString();
+        return  url.replace(request.getServletPath(), "");
+    }
+}
