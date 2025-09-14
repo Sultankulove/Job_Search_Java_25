c0eb5a52d9cf09af53dc4e2397d82ed6ea4aa625
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-14 23:50:24 +0600

    fix

--- src/main/java/kg/attractor/job_search_java_25/service/ProfileService.java
+++ src/main/java/kg/attractor/job_search_java_25/service/ProfileService.java
@@ -1,30 +1,35 @@
 package kg.attractor.job_search_java_25.service;
 
 import kg.attractor.job_search_java_25.dto.responseDto.RespondedApplicantDto;
+import kg.attractor.job_search_java_25.dto.resumeDtos.ResumeListItemDto;
 import kg.attractor.job_search_java_25.dto.userDtos.AvatarDto;
 import kg.attractor.job_search_java_25.dto.userDtos.EditProfileDto;
 import kg.attractor.job_search_java_25.dto.userDtos.UserProfileDto;
+import kg.attractor.job_search_java_25.dto.vacancyDtos.VacancyListItemDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ProfileService {
+
+    @Transactional
     void addAvatar(AvatarDto avatarDto);
 
     ResponseEntity<?> findAvatarById(Long userId);
 
-    UserProfileDto getMyProfile(Long auth);
+    UserProfileDto getMyProfile(Long id);
 
-    UserProfileDto updateProfileByUserId(EditProfileDto epd, Long auth);
+    @Transactional
+    UserProfileDto updateProfileByUserId(EditProfileDto dto, Long id);
 
-    ResponseEntity<List<RespondedApplicantDto>> getMyResponses(Long auth);
+    List<RespondedApplicantDto> getMyResponses(Long authUserId);
 
-    ResponseEntity<List<RespondedApplicantDto>> getMyVacanciesResponses(Long auth, Pageable p);
+    List<RespondedApplicantDto> getMyVacanciesResponses(Long authUserId, Pageable p);
 
-    List<VacancyShortDto> getMyShortVacancies(Long userId);
+    List<VacancyListItemDto> getMyShortVacancies(Long userId);
 
-    List<ResumeShortDto> getMyShortResumes(Long userId);
+    List<ResumeListItemDto> getMyShortResumes(Long userId);
 
-    UserProfileDto getUserProfile(Long id);
 }
