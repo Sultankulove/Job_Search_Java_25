9029e4d982592ba1398a134d9338109e3a7467bf
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-14 21:12:34 +0600

    fix

--- src/main/java/kg/attractor/job_search_java_25/service/ChatService.java
+++ src/main/java/kg/attractor/job_search_java_25/service/ChatService.java
@@ -1,11 +1,14 @@
 package kg.attractor.job_search_java_25.service;
 
 import kg.attractor.job_search_java_25.dto.messageDto.MessageDto;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ChatService {
-    List<MessageDto> getChatByResponseId(Long responseId);
 
-    void sendMessage(Long chatId, MessageDto dto);
+    List<MessageDto> getChatByResponseId(Long responseId, Long userId);
+
+    @Transactional
+    void sendMessage(Long responseId, Long userId, MessageDto dto);
 }
