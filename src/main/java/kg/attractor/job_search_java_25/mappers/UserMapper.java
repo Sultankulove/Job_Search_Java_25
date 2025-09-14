472222f7f91c2e734634d90346d094307c1f141c
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-14 21:05:08 +0600

    fix

--- src/main/java/kg/attractor/job_search_java_25/mappers/UserMapper.java
+++ src/main/java/kg/attractor/job_search_java_25/mappers/UserMapper.java
@@ -1,42 +1,33 @@
 package kg.attractor.job_search_java_25.mappers;
 
-import jakarta.persistence.*;
 import kg.attractor.job_search_java_25.dto.userDtos.AccountType;
+import kg.attractor.job_search_java_25.dto.userDtos.EditProfileDto;
 import kg.attractor.job_search_java_25.dto.userDtos.UserProfileDto;
-import kg.attractor.job_search_java_25.model.Resume;
 import kg.attractor.job_search_java_25.model.User;
-import kg.attractor.job_search_java_25.model.Vacancy;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Component
 public class UserMapper {
 
 
-    private String name;
-    private String surname;
-    private Byte age;
-    private String email;
-    private String phoneNumber;
-    private String avatar;
-    private String accountType;
-
-    public static UserProfileDto toDto(User user) {
-        if (user == null) return null;
-
-        UserProfileDto dto = new UserProfileDto();
-        dto.setName(user.getName());
-        dto.setSurname(user.getSurname());
-        dto.setAge(user.getAge());
-        dto.setEmail(user.getEmail());
-        dto.setPhoneNumber(user.getPhoneNumber());
-        dto.setAvatar(user.getAvatar());
-        dto.setAccountType(AccountType.valueOf(user.getAccountType()));
+    public UserProfileDto toProfile(User u) {
+        if (u == null) return null;
+        var dto = new UserProfileDto();
+        dto.setName(u.getName());
+        dto.setSurname(u.getSurname());
+        dto.setAge(u.getAge());
+        dto.setEmail(u.getEmail());
+        dto.setPhoneNumber(u.getPhoneNumber());
+        dto.setAvatar(u.getAvatar());
+        dto.setAccountType(AccountType.valueOf(u.getAccountType()));
         return dto;
     }
 
-    public static List<UserProfileDto> toDtoList(List<User> users) {
-        return users.stream()
-                .map(UserMapper::toDto)
-                .toList();
+    public void applyEdit(EditProfileDto d, User u) {
+        u.setName(d.getName());
+        u.setSurname(d.getSurname());
+        u.setAge(d.getAge());
+        u.setEmail(d.getEmail());
+        u.setPhoneNumber(d.getPhoneNumber());
     }
 }
