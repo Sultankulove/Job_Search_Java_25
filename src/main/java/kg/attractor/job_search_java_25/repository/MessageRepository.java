4a95ad47b489c52ee1ec279dd04011ad980ccd92
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-16 17:45:09 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/repository/MessageRepository.java
+++ src/main/java/kg/attractor/job_search_java_25/repository/MessageRepository.java
@@ -2,16 +2,13 @@ package kg.attractor.job_search_java_25.repository;
 
 import kg.attractor.job_search_java_25.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    List<Message> findByRespondedApplicant_IdOrderByTimestampAsc(Long responseId);
 
     List<Message> getMessagesByRespondedApplicant_IdOrderByTimestampAsc(Long respondedApplicantId);
 
