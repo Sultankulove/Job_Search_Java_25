7c3ee24434591659d5f2689c102e377565876bc5
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-16 17:51:17 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/repository/UserRepository.java
+++ src/main/java/kg/attractor/job_search_java_25/repository/UserRepository.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
 
     Optional<User> findByResetPasswordToken(String token);
     Optional<User> findByEmail(String email);
