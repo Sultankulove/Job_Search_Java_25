1d95efef1820b01038d30515d4fc6dca761b8293
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-16 17:30:32 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/repository/WorkExperienceInfoRepository.java
+++ src/main/java/kg/attractor/job_search_java_25/repository/WorkExperienceInfoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkExperienceInfoRepository extends JpaRepository<WorkExperienceInfo, Integer> {
+public interface WorkExperienceInfoRepository extends JpaRepository<WorkExperienceInfo, Long> {
     void deleteWorkExperienceInfoById(Long id);
 }
--- src/main/java/kg/attractor/job_search_java_25/service/impl/WorkExperienceInfoServiceImpl.java
+++ src/main/java/kg/attractor/job_search_java_25/service/impl/WorkExperienceInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package kg.attractor.job_search_java_25.service.impl;
 
 import kg.attractor.job_search_java_25.dto.resumeDtos.nested.WorkExperienceInfoDto;
+import kg.attractor.job_search_java_25.model.Resume;
 import kg.attractor.job_search_java_25.model.WorkExperienceInfo;
 import kg.attractor.job_search_java_25.repository.WorkExperienceInfoRepository;
 import kg.attractor.job_search_java_25.service.WorkExperienceInfoService;
@@ -14,11 +15,15 @@ public class WorkExperienceInfoServiceImpl implements WorkExperienceInfoService 
 
     public void create(WorkExperienceInfoDto workExperienceInfoDto, Long resumeId) {
         WorkExperienceInfo model = new WorkExperienceInfo();
-        model.getResume().setId(resumeId);
+        Resume resumeRef = new Resume();
+        resumeRef.setId(resumeId);
+        model.setResume(resumeRef);
+
         model.setYears(workExperienceInfoDto.getYears());
         model.setCompanyName(workExperienceInfoDto.getCompanyName());
         model.setPosition(workExperienceInfoDto.getPosition());
         model.setResponsibilities(workExperienceInfoDto.getResponsibilities());
+
         workExperienceInfoRepository.save(model);
     }
 }
