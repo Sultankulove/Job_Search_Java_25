93fa4c961a8473458e339d85b52bcbc7fd5a28d1
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-16 17:49:00 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/repository/EducationInfoRepository.java
+++ src/main/java/kg/attractor/job_search_java_25/repository/EducationInfoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EducationInfoRepository extends JpaRepository<EducationInfo, Integer> {
+public interface EducationInfoRepository extends JpaRepository<EducationInfo, Long> {
 
 }
