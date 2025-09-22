ed167fa01d4dcf7ced9a5bb99c4425d93e040e28
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-22 12:24:04 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/dto/resumeDtos/nested/contactDtos/ContactInfoDto.java
+++ src/main/java/kg/attractor/job_search_java_25/dto/resumeDtos/nested/contactDtos/ContactInfoDto.java
@@ -1,5 +1,8 @@
 package kg.attractor.job_search_java_25.dto.resumeDtos.nested.contactDtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ContactInfoDto {
-
+    @NotNull(message = "Выберите тип контакта")
     private Long typeId;
+
+
+    @NotBlank(message = "Заполните значение контакта")
+    @Size(max = 255)
     private String contactValue;
 
 }
