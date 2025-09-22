40185888a8798bed606ad104ea8d2f0dcb8c2e4c
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-22 12:24:24 +0600

    edit

--- src/main/java/kg/attractor/job_search_java_25/dto/resumeDtos/nested/EducationInfoDto.java
+++ src/main/java/kg/attractor/job_search_java_25/dto/resumeDtos/nested/EducationInfoDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -16,6 +17,10 @@ public class EducationInfoDto {
     private String institution;
     private String program;
     private String degree;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endDate;
 }
