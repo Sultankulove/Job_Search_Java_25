07244e2cc9be616f8971996e5aa604ceb9011675
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-27 16:53:48 +0600

    chat

--- src/main/java/kg/attractor/job_search_java_25/config/WebSocketSecurityConfig.java
+++ src/main/java/kg/attractor/job_search_java_25/config/WebSocketSecurityConfig.java
@@ -1,24 +1,33 @@
 package kg.attractor.job_search_java_25.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 @Configuration
 @EnableWebSocketSecurity
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager(
+            MessageMatcherDelegatingAuthorizationManager.Builder messages) {
         messages
+                .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.DISCONNECT, SimpMessageType.HEARTBEAT).permitAll()
                 .simpDestMatchers("/app/**").authenticated()
-                .simpSubscribeDestMatchers("/user/**", "/topic/**").authenticated()
+                .simpSubscribeDestMatchers("/user/**", "/topic/**", "/queue/**").authenticated()
                 .anyMessage().denyAll();
+
+        return messages.build();
     }
 
-//    @Override
-//    protected boolean sameOriginDisabled() {
-//        return true;
-//    }
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
 }
