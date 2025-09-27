3637b4b8fac00e048edb11b45dbc853e0a282cc0
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-27 18:54:16 +0600

    edit

--- src/main/resources/db/migration/V2__insert_into_users.sql
+++ src/main/resources/db/migration/V2__insert_into_users.sql
@@ -6,64 +6,64 @@ VALUES ('Admin', 'Root', 30, 'admin@example.com',
 
 -- Applicants
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Айбек', 'Султанов', 20, 'app1@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000001', 'APPLICANT', true);
+VALUES ('Айбек', 'Султанов', 20, 'app1@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000001', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Анна', 'Иванова', 20, 'app2@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000002', 'APPLICANT', true);
+VALUES ('Анна', 'Иванова', 20, 'app2@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000002', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Ержан', 'Касымов', 28, 'app3@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000003', 'APPLICANT', true);
+VALUES ('Ержан', 'Касымов', 28, 'app3@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000003', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Мария', 'Кузнецова', 35, 'app4@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000004', 'APPLICANT', true);
+VALUES ('Мария', 'Кузнецова', 35, 'app4@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000004', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Данияр', 'Абдрахманов', 28, 'app5@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000005', 'APPLICANT', true);
+VALUES ('Данияр', 'Абдрахманов', 28, 'app5@gmail.com', '$$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000005', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Ольга', 'Соколова', 35, 'ольга6@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000006', 'APPLICANT', true);
+VALUES ('Ольга', 'Соколова', 35, 'ольга6@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000006', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Нурсултан', 'Жапаров', 22, 'нурсултан7@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000007', 'APPLICANT', true);
+VALUES ('Нурсултан', 'Жапаров', 22, 'нурсултан7@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000007', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Ирина', 'Смирнова', 23, 'ирина8@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000008', 'APPLICANT', true);
+VALUES ('Ирина', 'Смирнова', 23, 'ирина8@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000008', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Алексей', 'Воробьев', 33, 'алексей9@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000009', 'APPLICANT', true);
+VALUES ('Алексей', 'Воробьев', 33, 'алексей9@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000009', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Жанара', 'Тургунова', 34, 'жанара10@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000010', 'APPLICANT', true);
+VALUES ('Жанара', 'Тургунова', 34, 'жанара10@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000010', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Павел', 'Романов', 26, 'павел11@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000011', 'APPLICANT', true);
+VALUES ('Павел', 'Романов', 26, 'павел11@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000011', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Асем', 'Жумабаева', 30, 'асем12@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000012', 'APPLICANT', true);
+VALUES ('Асем', 'Жумабаева', 30, 'асем12@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000012', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Кирилл', 'Федоров', 28, 'кирилл13@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000013', 'APPLICANT', true);
+VALUES ('Кирилл', 'Федоров', 28, 'кирилл13@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000013', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Гульмира', 'Абдукадырова', 30, 'гульмира14@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000014', 'APPLICANT', true);
+VALUES ('Гульмира', 'Абдукадырова', 30, 'гульмира14@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000014', 'APPLICANT', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Максим', 'Орлов', 30, 'максим15@mail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996701000015', 'APPLICANT', true);
+VALUES ('Максим', 'Орлов', 30, 'максим15@mail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996701000015', 'APPLICANT', true);
 
 -- Employers
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Айдар', 'Кулубаев', 48, 'emp1@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000001', 'EMPLOYER', true);
+VALUES ('Айдар', 'Кулубаев', 48, 'emp1@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000001', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Светлана', 'Петрова', 41, 'emp2@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000002', 'EMPLOYER', true);
+VALUES ('Светлана', 'Петрова', 41, 'emp2@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000002', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Арман', 'Жаксылыков', 50, 'emp3@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000003', 'EMPLOYER', true);
+VALUES ('Арман', 'Жаксылыков', 50, 'emp3@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000003', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Елена', 'Николаева', 51, 'emp4@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000004', 'EMPLOYER', true);
+VALUES ('Елена', 'Николаева', 51, 'emp4@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000004', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Бакыт', 'Осмонов', 32, 'emp5@gmail.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000005', 'EMPLOYER', true);
+VALUES ('Бакыт', 'Осмонов', 32, 'emp5@gmail.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000005', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Дмитрий', 'Сергеев', 39, 'дмитрий6@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000006', 'EMPLOYER', true);
+VALUES ('Дмитрий', 'Сергеев', 39, 'дмитрий6@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000006', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Жанат', 'Мухамедьяров', 52, 'жанат7@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000007', 'EMPLOYER', true);
+VALUES ('Жанат', 'Мухамедьяров', 52, 'жанат7@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000007', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Виктория', 'Савельева', 47, 'виктория8@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000008', 'EMPLOYER', true);
+VALUES ('Виктория', 'Савельева', 47, 'виктория8@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000008', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Андрей', 'Калинин', 38, 'андрей9@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000009', 'EMPLOYER', true);
+VALUES ('Андрей', 'Калинин', 38, 'андрей9@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000009', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Наталья', 'Шевченко', 31, 'наталья10@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000010', 'EMPLOYER', true);
+VALUES ('Наталья', 'Шевченко', 31, 'наталья10@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000010', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Игорь', 'Поляков', 38, 'игорь11@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000011', 'EMPLOYER', true);
+VALUES ('Игорь', 'Поляков', 38, 'игорь11@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000011', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Зарина', 'Умбетова', 48, 'зарина12@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000012', 'EMPLOYER', true);
+VALUES ('Зарина', 'Умбетова', 48, 'зарина12@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000012', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Руслан', 'Ахметов', 44, 'руслан13@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000013', 'EMPLOYER', true);
+VALUES ('Руслан', 'Ахметов', 44, 'руслан13@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000013', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Валерия', 'Григорьева', 40, 'валерия14@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000014', 'EMPLOYER', true);
+VALUES ('Валерия', 'Григорьева', 40, 'валерия14@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000014', 'EMPLOYER', true);
 INSERT INTO users (name, surname, age, email, password, phone_number, account_type, enabled)
-VALUES ('Оскар', 'Мирзаев', 52, 'оскар15@company.com', '$2a$10$IFds1Vn5pRK8tEOn6i8oY.P5Z5Q7mp4lcwRmU7DdrUhH6rULrw3v6', '+996702000015', 'EMPLOYER', true);
+VALUES ('Оскар', 'Мирзаев', 52, 'оскар15@company.com', '$2a$10$K6yJAC/3FNRmNOIimarqNOqJpo/i0h6P43rKgxQ0hP2ZBBLl5Lgiu', '+996702000015', 'EMPLOYER', true);
